6203fffadbf409d699ee8d2b8be551d40b87230e
anton-1108 <antonamarjargal27@gmail.com>
2025-05-16 02:54:22 +0800

    day1

---
+++ src/app/_components/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <div className="flex flex-col gap-2 sm:flex-row py-6 w-full border-t items-center justify-center px-4 md:px-6">
+      <p>Amarjargal Anton. All rights reserved</p>
+    </div>
+  );
+}
